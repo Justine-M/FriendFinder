b25f771a9c8b4a43194653d2be3a5107dc203362
Justine-M <justalexmays@hotmail.com>
2018-10-31 23:54:41 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,181 @@
+var friendsArray = [
+  {
+    "name":"Jake",
+    "photo":"images/jake.png",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        4,
+        5,
+        4,
+        1
+      ],
+    },
+
+  {
+
+      "name":"Garfield",
+      "photo":"images/garfield.png",
+      "scores":[
+          4,
+          2,
+          3,
+          1,
+          5,
+          1,
+          3,
+          5,
+          4,
+          1
+        ],
+
+      },
+
+      {
+        "name":"Finn",
+        "photo":"images/finn.png",
+        "scores":[
+            3,
+            4,
+            2,
+            4,
+            3,
+            1,
+            1,
+            5,
+            4,
+            2
+          ],
+      },
+
+      {
+        "name":"Wonder Woman",
+        "photo":"images/wonderwoman.jpg",
+        "scores":[
+            5,
+            3,
+            4,
+            3,
+            1,
+            2,
+            4,
+            5,
+            4,
+            2
+          ],
+      },
+
+      {
+        "name":"Harry Potter",
+            "photo":"images/harrypotter.jpg",
+            "scores":[
+                5,
+                4,
+                2,
+                4,
+                5,
+                1,
+                3,
+                3,
+                2,
+                1
+              ],
+      },
+
+    {
+      "name":"Storm",
+      "photo":"images/storm.jpg",
+      "scores":[
+          2,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          3,
+          3
+        ],
+    },
+
+    {
+      "name":"Goku",
+      "photo":"images/ssgoku.jpg",
+      "scores":[
+          2,
+          1,
+          4,
+          3,
+          5,
+          1,
+          2,
+          5,
+          1,
+          4
+        ],
+    },
+
+    {
+      "name":"Vegeta",
+      "photo":"images/ssvegeta.jpg",
+      "scores":[
+          4,
+          1,
+          4,
+          3,
+          5,
+          1,
+          2,
+          5,
+          3,
+          5
+        ],
+    },
+
+    {
+
+      "name":"Sailor Moon",
+      "photo":"images/saliormoon.jpg",
+      "scores":[
+          5,
+          1,
+          4,
+          3,
+          5,
+          1,
+          2,
+          5,
+          3,
+          2
+        ],
+    },
+
+    {                      
+    "name":"Maleficent",
+    "photo":"images/maleficent.jpg",
+    "scores":[
+        2,
+        1,
+        4,
+        5,
+        5,
+        4,
+        1,
+        4,
+        5,
+        1
+      ],
+    },
+
+
+  ]
+
+  // Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
+
+
